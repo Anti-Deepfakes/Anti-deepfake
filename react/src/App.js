79146d80cd17bc11100e49d7f7dff5cd922228af
강윤서 <ysys1130@gmail.com>
2024-10-30 15:30:15 +0900

    feat: detect & disturb 프로젝트 세팅

---
+++ react/src/App.js
@@ -0,0 +1,29 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Upload from './components/detect/Upload';
+import Result from './components/detect/Result';
+import Predict from './components/detect/Predict';
+import Main from './components/disrupt/Main';
+import Compare from './components/disrupt/Compare';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/disrupt/upload" element={<Main />} />
+        <Route path="/disrupt/compare" element={<Compare />} />
+
+        <Route path="/detect/upload" element={<Upload />} />
+        <Route path="/detect/predict" element={<Predict />} />
+        <Route path="/detect/result" element={<Result />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
