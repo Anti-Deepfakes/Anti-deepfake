cfb7d042cdd361a82136a42ea533029db0960925
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-11-15 13:56:12 +0900

    Update db.py

--- ffastapi/db.py
+++ ffastapi/db.py
@@ -9,7 +9,9 @@ passwd = os.getenv("DB_PASSWD") # "1234"
 host = os.getenv("DB_HOST")     # "127.0.0.1"
 port = os.getenv("DB_PORT", "3306")     # "3306"
 db = os.getenv("DB_NAME")       # "mydb"
-
+print(f"DB_USER: {os.getenv('DB_USER')}")
+print(f"DB_HOST: {os.getenv('DB_HOST')}")
+print(f"DB_PORT: {os.getenv('DB_PORT')}")
 DB_URL = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset=utf8'
 
 engine = create_engine(DB_URL, echo=True)
