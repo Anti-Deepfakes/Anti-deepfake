d5cb19a3c2e39eba16ab23ac818ddd3e9f13ebff
강윤서 <ysys1130@gmail.com>
2024-11-15 17:39:35 +0900

    style: 레이아웃 수정

--- android/app/src/main/java/com/antideepfake/android/MainActivity.java
+++ android/app/src/main/java/com/antideepfake/android/MainActivity.java
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         // "Upload Photo" 아이템에 애니메이션 적용
-        View uploadItem = navView.findViewById(R.id.navigation_upload);
-        Animation bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce);
-        uploadItem.startAnimation(bounceAnimation);
+//        View uploadItem = navView.findViewById(R.id.navigation_upload);
+//        Animation bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce);
+//        uploadItem.startAnimation(bounceAnimation);
 
         // 네비게이션을 구성하고 NavController 설정
         // (기본적으로 NavController 설정을 위한 코드가 추가되어야 함)
@@ -34,9 +34,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(navView, navController);
 
-        // ActionBar 숨기기 (원하는 경우)
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
     }
 }
