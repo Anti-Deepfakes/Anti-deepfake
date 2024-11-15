daee9caf6e30ed5a2d09a792276df2c986f7a646
장현준 <gapple95@naver.com>
2024-11-15 14:53:47 +0900

    fix: CORS 설정 추가

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
+from starlette.middleware.cors import CORSMiddleware
+
 from controller.detect_controller import router as image_router
 from fastapi.responses import JSONResponse
 from model.detect_model_loader import load_multimodal_models
@@ -9,9 +11,19 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 app = FastAPI()
+# CORS 설정
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
