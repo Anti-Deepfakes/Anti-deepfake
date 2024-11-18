c30be1f2ba96fa092e025bf7f6234c1e5b07e82d
dldudnjs198 <dlduddnjs198@naver.com>
2024-11-18 12:51:56 +0900

    refactor: mlflow 저장경로 수정

--- app/utils/train.py
+++ app/utils/train.py
@@ -216,6 +216,6 @@ def train(hp, train_loader, valid_loader, chkpt_path, save_dir, db, version, dat
 
     mlflow.pytorch.log_model(
         pytorch_model=perturbation_generator,
-        artifact_path="models"
+        artifact_path="/mlflow/model"
     )
     print("[LOG: train] Model logged to MLflow.")
