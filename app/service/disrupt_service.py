a4b91bf31a24cb56a645b6ea20adc77e67b8d28b
정재희 <wjfwogml5890@naver.com>
2024-11-18 23:01:36 +0900

    chore: print 삭제

--- app/service/disrupt_service.py
+++ app/service/disrupt_service.py
@@ -28,12 +28,6 @@ async def generate_disrupt_v1(model, image, device, face_detector):
             
             perturbations = model(image_weighted)
             perturbations = F.interpolate(perturbations, size=(image_tensor.shape[2], image_tensor.shape[3]), mode='area')
-            print(image_tensor)
-            
-            # target_rgb = (255, 204, 153)
-            # perturbations[:, 0, ...] *= torch.tensor(target_rgb[0] / 255.0).float().cuda()
-            # perturbations[:, 1, ...] *= torch.tensor(target_rgb[1] / 255.0).float().cuda()
-            # perturbations[:, 2, ...] *= torch.tensor(target_rgb[2] / 255.0).float().cuda()
 
             perturbed_images = image_tensor + (perturbations + (2-weighted_map)*0.25)
             perturbed_images += image_tensor
@@ -42,14 +36,6 @@ async def generate_disrupt_v1(model, image, device, face_detector):
             max_val = perturbed_images.max()
 
             perturbed_images = (perturbed_images - min_val) / (max_val - min_val)
-
-            # perturbed_images += image_tensor
-            # min_val = perturbed_images.min()
-            # max_val = perturbed_images.max()
-
-            # perturbed_images = image_tensor * (perturbations-1)
-            print(perturbed_images)
-            # perturbed_images = torch.clamp(perturbed_images, min=0.0, max=1.0)
 
             final_image = await tensor_to_cv2_image(perturbed_images)
             
@@ -61,7 +47,7 @@ async def generate_disrupt_v1(model, image, device, face_detector):
             pil_image.save(img_byte_arr, format='JPEG')
             img_byte_arr.seek(0)
             
-            cv2.imwrite("./disrupt_image.jpg", final_image)
+            # cv2.imwrite("./disrupt_image.jpg", final_image)
         os.remove(image_path)
 
     except Exception as e:
