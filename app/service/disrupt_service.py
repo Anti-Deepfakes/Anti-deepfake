d8e1c33eaa35c96a4d3c5893cf9ae49052558c50
정재희 <wjfwogml5890@naver.com>
2024-11-18 22:59:20 +0900

    feat: image post precess

--- app/service/disrupt_service.py
+++ app/service/disrupt_service.py
@@ -16,23 +16,43 @@ import torch
 import torch.fft
 import numpy as np
 
-
 async def generate_disrupt_v1(model, image, device, face_detector):
     image_path = await save_image(image)
+      
 
-    image_tensor, image_weighted = await preprocessing(image_path, face_detector)
+    image_tensor, image_weighted, weighted_map = await preprocessing(image_path, face_detector)
 
     try:
 
         with torch.no_grad():
-
+            
             perturbations = model(image_weighted)
-            perturbations = F.interpolate(perturbations, size=(image_tensor.shape[2], image_tensor.shape[3]),
-                                          mode='area')
-            perturbed_images = image_tensor + perturbations
+            perturbations = F.interpolate(perturbations, size=(image_tensor.shape[2], image_tensor.shape[3]), mode='area')
+            print(image_tensor)
+            
+            # target_rgb = (255, 204, 153)
+            # perturbations[:, 0, ...] *= torch.tensor(target_rgb[0] / 255.0).float().cuda()
+            # perturbations[:, 1, ...] *= torch.tensor(target_rgb[1] / 255.0).float().cuda()
+            # perturbations[:, 2, ...] *= torch.tensor(target_rgb[2] / 255.0).float().cuda()
+
+            perturbed_images = image_tensor + (perturbations + (2-weighted_map)*0.25)
+            perturbed_images += image_tensor
+
+            min_val = perturbed_images.min()
+            max_val = perturbed_images.max()
+
+            perturbed_images = (perturbed_images - min_val) / (max_val - min_val)
+
+            # perturbed_images += image_tensor
+            # min_val = perturbed_images.min()
+            # max_val = perturbed_images.max()
+
+            # perturbed_images = image_tensor * (perturbations-1)
+            print(perturbed_images)
+            # perturbed_images = torch.clamp(perturbed_images, min=0.0, max=1.0)
 
             final_image = await tensor_to_cv2_image(perturbed_images)
-
+            
             final_image_rgb = cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)
 
             pil_image = Image.fromarray(final_image_rgb)
@@ -40,8 +60,8 @@ async def generate_disrupt_v1(model, image, device, face_detector):
             img_byte_arr = BytesIO()
             pil_image.save(img_byte_arr, format='JPEG')
             img_byte_arr.seek(0)
-
-            # cv2.imwrite("./disrupt_image.jpg", final_image)
+            
+            cv2.imwrite("./disrupt_image.jpg", final_image)
         os.remove(image_path)
 
     except Exception as e:
--- app/service/image_processing.py
+++ app/service/image_processing.py
@@ -5,7 +5,6 @@ import torch
 from fastapi import HTTPException
 import torch.nn.functional as F
 
-
 async def save_image(image):
     try:
         os.makedirs("./data", exist_ok=True)
@@ -21,44 +20,53 @@ async def save_image(image):
 
     return image_path
 
-
 async def preprocessing(image_path, face_detector):
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # image_resized = cv2.resize(image_rgb, (224, 224))
+    height, width = image.shape[:2]
+    
+    max_dimension = max(height, width)
 
+    if max_dimension > 1000:
+        scale_ratio = 1000 / max_dimension
+        
+        new_width = int(width * scale_ratio)
+        new_height = int(height * scale_ratio)
+        
+        image_rgb = cv2.resize(image_rgb, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    
     image_normalized = image_rgb.astype(np.float32) / 255.0
     image_normalized = np.transpose(image_normalized, (2, 0, 1))  # (H, W, C) -> (C, H, W)
-
+    
     faces = face_detector.get(image)
     # print(faces)
     # print(F.softmax
     # print(faces.shape)
-
+    
     try:
         face = faces[0]
         bbox = face['bbox']
         landmarks = face['landmark_3d_68']
-
+        
     except Exception as e:
         print(e, " : ", img_name)
-
+        
         bbox = [0, 0, 0, 0]
         landmarks = []
 
     weight_map = np.zeros(image_normalized.shape[1:], dtype=np.float32)
     weight_map = await add_weight_to_bbox(weight_map, bbox)
     weight_map = await add_weight_to_landmarks(weight_map, landmarks)
-
-    lansmarks = np.array(landmarks, dtype=np.float32) if len(landmarks) > 0 else np.zeros((68, 3), dtype=np.float32)
-
+    
+    lansmarks=np.array(landmarks, dtype=np.float32) if len(landmarks) > 0 else np.zeros((68, 3), dtype=np.float32)
+   
     image_tensor = torch.tensor(image_normalized).float().cuda()
-
+    
     weight_map_tensor = torch.tensor(weight_map).float().cuda()
-
+    
     image_weighted = image_tensor + torch.stack([weight_map_tensor] * 3)
 
-    return image_tensor.unsqueeze(0), image_weighted.unsqueeze(0)
+    return image_tensor.unsqueeze(0), image_weighted.unsqueeze(0), torch.stack([weight_map_tensor]*3).unsqueeze(0)
 
 
 async def add_weight_to_bbox(weight_map, bbox):
@@ -72,10 +80,8 @@ async def add_weight_to_landmarks(weight_map, landmarks):
     if len(landmarks) > 0:
         for (x, y, _) in landmarks:
             x, y = int(x), int(y)
-            weight_map[max(0, y - 5):min(weight_map.shape[0], y + 5),
-            max(0, x - 5):min(weight_map.shape[1], x + 5)] = 2.0  # 랜드마크 주변에 가중치 2 부여
+            weight_map[max(0, y-5):min(weight_map.shape[0], y+5), max(0, x-5):min(weight_map.shape[1], x+5)] = 2.0  # 랜드마크 주변에 가중치 2 부여
     return weight_map
-
 
 async def tensor_to_cv2_image(tensor_image):
     image_np = tensor_image.squeeze(0).permute(1, 2, 0).cpu().detach().numpy() * 255.0
